8f7f4b673f11dc0b7beaae4eea5e40bcd0abea08
RakeshKumarParida <rakeshkumarparida424@gmail.com>
2022-11-08 13:32:31 +0530

    Changes is js file

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
+
+
+
 let calculate = () => {
-    let date = document.getElementById('date').value;
-    let month = document.getElementById('month').value;
-    let year = document.getElementById('year').value;
     
-    if (date == 0 || date == "") {
-        alert("Date field can not be blancked !");
+    let date = document.getElementById("date").value;
+    let month = document.getElementById("month").value;
+    let year = document.getElementById("year").value;
+
+    // console.log(date + '/' + month + '/' + year);
+
+    if (date == 0 || date == '') {
+        alert("date field can not be blancked !");
         return;
     }
-    if (year == 0 || year == "") {
-        alert("Year field can not be blancked !");
+    if (year == 0 || year == '') {
+        alert('year field can not be blancked !');
         return;
     }
 
-    let currentDate = new Date();
-    let dob = new Date(`${date}  ${month} ${year}`);
-    
-    let diffrence = (currentDate - dob);
+    let userDate = new Date();
+    let dob = new Date(`${date} ${month} ${year}`);
+    console.log(dob);
 
-    let ageIs = Math.round(diffrence / (1000 * 60 * 60 * 24) / 365);
-    alert(`Your age is ${ageIs}`);
+    let diffrence = (userDate - dob);
+    console.log(diffrence);
+
+    let finalAge = Math.floor(diffrence / (1000 * 60 * 60 * 24) / 365 );  
+    console.log( finalAge);
+
+
+
 }
